6e4154f171abda17465180578a720f617be4345c
BenBasic <benjaminmbasic@gmail.com>
2022-06-17 18:43:37 -0400

    Added comments to html.js and server.js, got heroku link to work so Ill be adding to the readme to reflect that

--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,14 @@
+// Assigning node package requirements
 const path = require('path');
 
-
+// Exporting the module to be used within server.js
 module.exports = function(app) {
-
+    // Getting index.html information
     app.get("/", function(request, response) {
         response.sendFile(path.join(__dirname, "../public/assets/index.html"));
     });
 
-    
+    // Getting notes.html information
     app.get("/notes", function(request, response) {
         response.sendFile(path.join(__dirname, "../public/notes.html"));
     });
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+// Assigning node package requirements
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const path = require("path");
 
+// Assigning the port to either be the process environment port, or a port 3001 for local host
 const PORT = process.env.PORT || 3001;
 
+// Initializing the express
 app.use(express.json())
 app.use(express.urlencoded({
     extended:true
 }));
 app.use(express.static("public"));
 
-
+// Setting up the requirements for using the exported function within api.js and html.js
 require("./routes/api.js")(app);
 require("./routes/html.js")(app);
 
+// Assigning the express to listen for the port and console logging it to the terminal
 app.listen(PORT, function() {
     console.log("Listening to port: " + PORT);
 });
