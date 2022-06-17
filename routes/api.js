d76f91b86534cc037afd6c8a03f6f10fcaa8f50b
BenBasic <benjaminmbasic@gmail.com>
2022-06-17 00:33:25 -0400

    Created app.delete functionality in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,34 @@
 const fs = require("fs");
 const path = require("path");
 
-app.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname,"./public/notes.html"))
-})
+module.exports = function(app) {
 
-app.get("/api/notes", function(request, response) {
-    response.sendFile(path.join(__dirname,"./db/db.json"))
-})
-
-app.post("/api/notes", function(request, response) {
-    const addNote = request.body;
-    addNote.id = Date.now();
-    const data = fs.readFileSync("./db/db.json", "utf8");
-    const jsonData = JSON.parse(data);
-    fs.writeFileSync("./db/db.json", JSON.stringify(jsonData));
-    response.send();
-});
+    app.get("/notes", function(request, response) {
+        response.sendFile(path.join(__dirname,"./public/notes.html"))
+    })
+    
+    app.get("/api/notes", function(request, response) {
+        response.sendFile(path.join(__dirname,"./db/db.json"))
+    })
+    
+    app.post("/api/notes", function(request, response) {
+        const addNote = request.body;
+        addNote.id = Date.now();
+        const data = fs.readFileSync("./db/db.json", "utf8");
+        const jsonData = JSON.parse(data);
+        fs.writeFileSync("./db/db.json", JSON.stringify(jsonData));
+        response.send();
+    });
+    
+    app.delete("/api/notes/:id", function(request, response) {
+        const noteId = Number(request.params.id);
+        const data = fs.readFileSync("./db/db.json", "utf8");
+        const jsonData = JSON.parse(data);
+        const newNoteJSON = jsonData.filter(function(thisNote) {
+            return thisNote.id !== noteId;
+        });
+    
+        fs.writeFileSync("./db/db.json", JSON.stringify(newNoteJSON));
+        response.send();
+    });
+};
